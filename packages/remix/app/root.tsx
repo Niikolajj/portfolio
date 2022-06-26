27cc3baac655304170f1e9b0cc6bb9ae47ed4c7b
Niikolajj <freetob3free@hotmail.com>
2022-06-26 19:41:01 +0200

    Update site title

--- packages/remix/app/root.tsx
+++ packages/remix/app/root.tsx
@@ -17,7 +17,7 @@ import { ServerStyleContext, ClientStyleContext } from "./context";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "undefined.dev",
   viewport: "width=device-width,initial-scale=1",
 });
 
