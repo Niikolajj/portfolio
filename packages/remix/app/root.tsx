def3839c916e7686f3f6e2ea6ba99823610794db
Niikolajj <freetob3free@hotmail.com>
2022-07-05 16:27:15 +0200

    Fix infinite rerender
    caused by a lint fix that added cache to the dependency array

--- packages/remix/app/root.tsx
+++ packages/remix/app/root.tsx
@@ -52,7 +52,7 @@ const Document = withEmotionCache(
       });
       // reset cache to reapply global styles
       clientStyleData?.reset();
-    }, [clientStyleData, emotionCache.sheet]);
+    }, []);
 
     return (
       <html lang="en">
