2f3daf42ac6601a97110f58b1397b74d3952d26d
Niikolajj <freetob3free@hotmail.com>
2022-11-09 01:42:30 +0100

    Fix recent song calculation
    - add timezone correction

--- packages/remix/app/hooks/useNowPlaying.ts
+++ packages/remix/app/hooks/useNowPlaying.ts
@@ -11,7 +11,7 @@ export default function useNowPlaying(username: string) {
         const result = await fetch("/api/nowPlaying/" + username)
         const songs: lastFMResponse = await result.json()
         if("recenttracks" in songs) {
-          if(("@attr" in songs.recenttracks.track[0] || ("date" in songs.recenttracks.track[0] && (new Date(songs.recenttracks.track[0].date["#text"]).getTime()) < (new Date().getTime() - 120 * 1000)))) {
+          if(("@attr" in songs.recenttracks.track[0] || ("date" in songs.recenttracks.track[0] && ((new Date(songs.recenttracks.track[0].date["#text"])).getTime()) > ((new Date()).getTime() - 65 * 60 * 1000)))) {
             setSong(songs.recenttracks?.track[0])
           }
         }
